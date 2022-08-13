e0366fc555e84876b4f384d4cde5c5c9014f425f
Andriy Ivanenko <andriy.ivanenko@gmail.com>
2022-08-13 18:01:06 +0300

    tasks 1,2 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,35 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+const gallery = document.querySelector('.gallery');
+let galleryMarkup = '';
+
+galleryItems.forEach(galleryItem => {
+  galleryMarkup += `<a 
+  class="gallery__item"
+  href="${galleryItem.original}">
+    <img
+      class="gallery__image"
+      src="${galleryItem.preview}"
+      alt="${galleryItem.description}"
+    />
+  </a>`;
+});
+gallery.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+gallery.addEventListener('click', onImageClick);
+function onImageClick(event) {
+  event.preventDefault();
+}
+
+// const SimpleLightbox = require('simplelightbox');
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const simpleLightboxGallery = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+
+//
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+// const Vimeo = require('@vimeo/player');
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
+
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
+
+if (!localStorage['videoplayer-current-time']) {
+  player.setCurrentTime(0);
+} else {
+  player.setCurrentTime(localStorage['videoplayer-current-time']);
+}
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+    // console.log(localStorage['videoplayer-current-time']);
+    // localStorage.clear();
+  }, 1000)
+);
